17940120962d342a89c0b0229c3d8cb6cca0cdc3
Lorena <loreninhazanon@gmail.com>
2024-02-07 11:26:37 -0300

    javascript-básico

---
+++ 01-variaveis.js
@@ -0,0 +1,12 @@
+//Criando variáveis com "var" (var nome variavel= <valor>;)
+
+var nome = "Lorena"; //var criar variavel global
+var idade = 15;
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos!");
+
+console.log( "Agora fiz aniversário! Tenho", idade+1);
+
+//aumentando automagicamente +1 (autoincremento)
+idade++  //idade+1 = idade +1;
+console.log("Agora fiz aniverário! Tenho", idade, "anos!");
---
+++ 02-variaveis.js
@@ -0,0 +1,26 @@
+//utilizando operadores aritmeticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+var x = 10;
+var y = 2;
+
+console.log("A soma entre", x, "e", y, "é:", x + y);
+
+var soma = x + y
+var subtracao = x - y
+var multiplicacao = x * y
+var divisao = x / y
+
+console.log("Soma =", soma)
+console.log("Subtracao =", subtracao)
+console.log("Multiplicacao =", multiplicacao)
+console.log("Divisao =", divisao)
+
+//Regras para nomear identificadores (var, const ou let)
+//Podem comecar com A-Z, a-z, _ ou $
+
+var MinhaVariavel = 10;
+var outraVariavel = 20;
+const _minhaConstante = 3.1514;
+var $minhaVariavel = 11;
