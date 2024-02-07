9f0f33adaca30977a594bfa47c225693c46f657e
Lorena <loreninhazanon@gmail.com>
2024-02-07 17:04:00 -0300

    Finalizadas atividades da aula

---
+++ 03-somandoTexto.js
@@ -0,0 +1,14 @@
+//somando texto
+
+var nome = "Lorenna";
+var boasVindas = "Olá " + nome + ", seja bem vindo!";
+var comVirgula = ("Olá ", nome, "seja bem vindo!");
+
+console.log(boasVindas);
+console.log("Olá", nome, "seja bem vindo!");
+
+var texto1 = "Olá";
+var texto2 = "Bom dia";
+
+console.log(texto1 + texto2);
+console.log(texto1 + ", " + texto2);
